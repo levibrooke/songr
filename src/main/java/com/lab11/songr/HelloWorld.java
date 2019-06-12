a934a7ef96d88b7e9887c550a4b040a319a4da62
Levi Porter <levi@levibrooke.com>
2019-06-12 12:00:28 -0700

    Change route for hello world

--- src/main/java/com/lab11/songr/HelloWorld.java
+++ src/main/java/com/lab11/songr/HelloWorld.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
 
     // route
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String getHelloWorld() {
         return "Hello, world!";
     }
