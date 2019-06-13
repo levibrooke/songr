a40f02d6140dd18718c2e4f40a65610ee8f33186
Levi Porter <levi@levibrooke.com>
2019-06-13 13:01:35 -0700

    Add new album form, add route to controller

--- src/main/java/com/lab11/songr/AlbumController.java
+++ src/main/java/com/lab11/songr/AlbumController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AlbumController {
@@ -16,5 +19,17 @@ public class AlbumController {
         Iterable<Album> albums = albumRepository.findAll();
         m.addAttribute("albums", albums);
         return "albums";
+    }
+
+    @GetMapping("/albums/new")
+    public String getNewAlbumForm() {
+        return "albumForm";
+    }
+
+    @PostMapping("/albums")
+    public RedirectView addAlbum(@RequestParam String title, String artist, int songCount, int length, String imageUrl) {
+        Album newAlbum = new Album(title, artist, songCount, length, imageUrl);
+        albumRepository.save(newAlbum);
+        return new RedirectView("/albums");
     }
 }
